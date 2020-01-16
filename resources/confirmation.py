713dc684beca02745d5bee612af04b74b0d559f4
Maxon Tayebwa <maxtayebwa@gmail.com>
2020-01-16 20:19:17 +0300

    debug(): recify bug in user schema (@pre_dump)

--- resources/confirmation.py
+++ resources/confirmation.py
@@ -1,8 +1,9 @@
-from time import time
 import traceback
+from time import time
+
 from flask import make_response, render_template
 from flask_restful import Resource
-from libs.mailgun import MailGunException
+from libs.mailgun import MailGunException~
 from models.confirmation import ConfirmationModel
 from models.user import UserModel
 from schemas.confirmation import ConfirmationSchema
